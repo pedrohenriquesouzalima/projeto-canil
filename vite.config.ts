4f0884669876b85698d00a29b2967548424d1a59
Pedro Henrique Souza Lima <pedrohenriquesolt@gmail.com>
2026-07-23 21:31:52 -0300

    atualizado no vite.config.ts
    atualizado no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+})import { defineConfig } from 'vite'
+import react from '@vitejs.plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/projeto-canil/', // <--- Adicione exatamente esta linha com o nome do seu repositório
 })
