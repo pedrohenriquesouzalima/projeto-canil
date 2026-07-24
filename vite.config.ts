f1fec4ba6887724154ab9b1bdf99a28ffa9c70c4
Pedro Henrique Souza Lima <pedrohenriquesolt@gmail.com>
2026-07-23 22:03:42 -0300

    deploy do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})import { defineConfig } from 'vite'
-import react from '@vitejs.plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/projeto-canil/', // <--- Adicione exatamente esta linha com o nome do seu repositório
+  base: '/projeto-canil/',
 })
