f1c846a484a6fa3e29041e151efb57ba68def312
Pedro Henrique Souza Lima <pedrohenriquesolt@gmail.com>
2026-07-31 14:00:27 -0300

    ateração no vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- Troque '/projeto-canil/' por './'
+  base: '/projeto-canil/', 
 })
